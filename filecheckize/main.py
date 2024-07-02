fce0f4e95f65e06a44c1f37253163f5094cbf7eb
Sasha Lopoukhine <superlopuh@gmail.com>
2024-07-02 16:49:15 +0100

    add --xdsl-anonymize option mutually exclusive with --mlir-anonymize (#2)
    Sometimes the names carry semantic meaning, so it's useful to check that
    they are preserved.

--- filecheckize/main.py
+++ filecheckize/main.py
@@ -18,10 +18,16 @@ def main():
         nargs="?",
         help="Input file to read. Defaults to standard input.",
     )
-    parser.add_argument(
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
         "--mlir-anonymize",
         action="store_true",
         help="Anonymize MLIR SSA value names and basic block names.",
+    )
+    group.add_argument(
+        "--xdsl-anonymize",
+        action="store_true",
+        help="Anonymize MLIR unnamed SSA values and basic block names.",
     )
     parser.add_argument(
         "--check-empty-lines",
@@ -49,6 +55,7 @@ def main():
     args = parser.parse_args(sys.argv[1:])
 
     comment_line = re.compile(rf"^\s*{re.escape(args.comments_prefix)}.*$")
+    unnamed_ssa_value = re.compile(r"%([\d]+)")
     ssa_value_name = re.compile(r"%([\d]+|[\w$._-][\w\d$._-]*)(:|#[\d]*)?")
     basic_block_name = re.compile(r"\^([\d]+|[\w$._-][\w\d$._-]*)")
 
@@ -73,9 +80,13 @@ def main():
             if re.match(comment_line, line):
                 continue
 
-        if args.mlir_anonymize:
-            # Anonymize SSA value names
-            line = re.sub(ssa_value_name, r"%{{.*}}", line)
+        if args.mlir_anonymize or args.xdsl_anonymize:
+            if args.mlir_anonymize:
+                # Anonymize SSA value names
+                line = re.sub(ssa_value_name, r"%{{.*}}", line)
+            elif args.xdsl_anonymize:
+                # Anonymize unnamed SSA values
+                line = re.sub(unnamed_ssa_value, r"%{{.*}}", line)
 
             # Anonymize basic blocks names
             line = re.sub(basic_block_name, r"^{{.*}}", line)
